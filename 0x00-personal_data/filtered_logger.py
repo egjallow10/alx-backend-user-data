bad070cd1c50d9633ef3350a3fe1a5ab89b5569b
egjallo <egjallo@insistglobal.com>
2023-04-26 16:10:21 +0000

    changes

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,5 +6,6 @@ import re
 
 
 def filter_datum(fields, redaction, message, separator):
+    """Funtion that return a string"""
     pattern = r'({0})=([^{1}]+)'.format('|'.join(fields), separator)
     return re.sub(pattern, r'\1={0}{1}'.format(redaction, separator), message)
