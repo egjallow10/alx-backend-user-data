9d0491b9e2a914171547c772bfa75a9bfd305701
egjallo <egjallo@insistglobal.com>
2023-05-02 16:50:01 +0000

    not full version:

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,13 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized', strict_slashes=False)
+def stats() -> str:
+    """
+    GET /api/vi/stats
+    Return:
+    String of unauthorised
+    """
+    abort(401, description="unauthorized")
