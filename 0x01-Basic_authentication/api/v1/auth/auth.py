71d889d96bcfb1fae1e32250f73cd50a1791267a
egjallo <egjallo@insistglobal.com>
2023-05-04 12:19:26 +0000

    not full version:

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -9,16 +9,17 @@ from typing import List, TypeVar
 class Auth:
     """ Auth class
     """
+
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ Method to require authentication
         """
-        
+
         return False
 
     def authorization_header(self, request=None) -> str:
         """ authorization_header method
         """
-        
+
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
