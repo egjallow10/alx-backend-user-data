08bb817aa4b6045f6c2aeb3ab69ba64890aada37
egjallo <egjallo@insistglobal.com>
2023-05-04 07:59:27 +0000

    not full version:

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -2,24 +2,26 @@
 """
 Class templete for all authorization
 """
-
-
 from flask import request
 from typing import List, TypeVar
 
 
-class Auth:
+class Auth():
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """
         Method to require authentication for a given path.
         """
+        if path is None or excluded_paths is None or not len(excluded_paths):
+            return True
+
         return False
 
     def authorization_header(self, request=None) -> str:
         """
         Method to get the authorization header from a Flask request.
         """
-        return None
+        if request is None:
+            return None
 
     def current_user(self, request=None) -> TypeVar('User'):
         """
