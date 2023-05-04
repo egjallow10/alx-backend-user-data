163aa02cc33bfbd390321189c2e6bba7df4f0179
egjallo <egjallo@insistglobal.com>
2023-05-04 08:47:34 +0000

    changes

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -9,15 +9,12 @@ from typing import List, TypeVar
 class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Method to require authentication"""
-        if path is None or excluded_paths is None or not len(excluded_paths):
-            return True
 
         return False
 
     def authorization_header(self, request=None) -> str:
         """Method to get the authorization header fr"""
-        if request is None:
-            return None
+        return None
 
     def current_user(self, request=None) -> TypeVar('User'):
         """Method to get the current user """
