b1809af487a9ccdaa3a0cc318814901ec4f69a9e
egjallow10 <egjallow10@gmail.com>
2023-05-11 13:52:35 +0000

    creing auth

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,6 +3,8 @@
 
 
 import bcrypt
+from db import DB
+from user import User
 
 
 def _hash_password(password: str) -> bytes:
@@ -10,3 +12,33 @@ def _hash_password(password: str) -> bytes:
     salt_pass = password.encode('utf-8')
     new_password = bcrypt.hashpw(salt_pass, bcrypt.gensalt())
     return new_password
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """ returns a new users
+
+        Args:
+            email (str): _description_
+            password (str): _description_
+
+        Raises:
+            ValueError: _description_
+
+        Returns:
+            User: _description_
+        """
+        try:
+            find_user = self._db.find_user_by(email=email)
+            if find_user:
+                raise ValueError("User {} already exits".format(email))
+        except BaseException:
+            hassed_password = _hash_password(password)
+            new_user = User(email=email, hashed_password=hassed_password)
+            return new_user
