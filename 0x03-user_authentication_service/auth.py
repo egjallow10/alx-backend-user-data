4b4c967282a2a27b4e998ee9cb7d05ec37ed812a
egjallow10 <egjallow10@gmail.com>
2023-05-11 12:20:36 +0000

    hashing

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -4,8 +4,9 @@
 
 import bcrypt
 
+
 def _hash_password(password: str) -> bytes:
     """salting password and hashing"""
     salt_pass = password.encode('utf-8')
-    new_password = bcrypt.hashpw(salt_pass,bcrypt.gensalt())
+    new_password = bcrypt.hashpw(salt_pass, bcrypt.gensalt())
     return new_password
