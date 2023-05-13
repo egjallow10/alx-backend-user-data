f3f9778e5d15df48a835133fcd6867b3cf9ad204
egjallow10 <egjallow10@gmail.com>
2023-05-13 12:48:52 +0000

    simple flash app

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -5,8 +5,10 @@
 from flask import Flask, jsonify
 app = Flask(__name__)
 
+
 @app.route('/')
 def home_page():
+    """simple basic flash app"""
     return jsonify({"message": "Bienvenue"})
 
 if __name__ == "__main__":
